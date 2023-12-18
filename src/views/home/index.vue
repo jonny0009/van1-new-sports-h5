54592bb05340906f6a442cbb1b4c01a27d889448
XY-Chaos <XY-Chaos@gmail.com>
2023-12-18 09:51:21 +0800

    first  go  project   gggg

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home-page">
+
+
+    
     <van-button type="primary">主要按钮</van-button>
     <van-button type="success">成功按钮</van-button>
     <van-button type="default">默认按钮</van-button>
