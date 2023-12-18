b336d91e4a447675ac0fe5054645b6aca21b14c3
Bevis <be69385@gmail.com>
2023-12-18 11:17:30 +0800

    update

---
+++ src/views/live/components/LiveItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="item">item</div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  item: Object
+})
+</script>
+
+<style lang="scss" scoped>
+.item {
+  width: 165px;
+  height: 220px;
+  background: #eff1f2;
+  border-radius: 10px;
+}
+</style>
--- src/views/live/index.vue
+++ src/views/live/index.vue
@@ -1,16 +1,78 @@
 <template>
   <div class="live-page">
-    <div v-for="item in 66" :key="item">test, {{ item }}</div>
+    <div class="tab">
+      <div class="tab-item" v-for="(item, i) in tabList" :key="i" :class="{ active: tabActive === item.value }">
+        <img :src="item.icon" alt="" />
+        <span>{{ item.name }}</span>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <LiveItem :item="{}" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import LiveItem from './components/LiveItem.vue'
 
+const tabList = reactive([
+  { value: 1, name: '热门', icon: getImage('icon_hot_on.png') },
+  { value: 2, name: '足球', icon: getImage('icon_bask_un.png') },
+  { value: 3, name: '篮球', icon: getImage('icon_bask_un.png') },
+  { value: 4, name: '网球', icon: getImage('icon_tens_un.png') },
+  { value: 4, name: '网球', icon: getImage('icon_tens_un.png') },
+  { value: 4, name: '网球', icon: getImage('icon_tens_un.png') }
+])
+const tabActive = ref(1)
+
+function getImage(name: string) {
+  return new URL(`/src/assets/images/live/${name}`, import.meta.url).href
+}
 </script>
 
 <style lang="scss" scoped>
 .live-page {
   height: 100%;
+  overflow-y: auto;
 }
-
+.tab {
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  display: flex;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  &-item {
+    min-width: 76px;
+    height: 32px;
+    padding: 0 5px;
+    background: #eff1f2;
+    border-radius: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #1f2630;
+    font-size: 12px;
+    font-family: PingFangSC-Semibold, SF-Pro-Bold, system-ui;
+    font-weight: 800;
+    transition: all 0.3s;
+    &:not(:last-child) {
+      margin-right: 8px;
+    }
+    > img {
+      width: 18px;
+      margin-right: 7px;
+    }
+    &.active {
+      background: #7643fd;
+      color: #fff;
+    }
+  }
+}
+.wrapper {
+}
 </style>
