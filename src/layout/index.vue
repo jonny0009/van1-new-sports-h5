228af6adc44afa35ce2ea1abffc22c6d66d479be
XY-Chaos <XY-Chaos@gmail.com>
2023-12-18 17:17:11 +0800

    全局  底部 设计

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,13 @@
 <template>
+  <GlobalHeader />
   <AppMain />
   <GlobalFooter />
 </template>
 <script lang="ts" setup>
+import GlobalHeader from './components/GlobalHeader/index.vue'
 import AppMain from './components/AppMain.vue'
 import GlobalFooter from './components/GlobalFooter/index.vue'
+
 </script>
 <style lang="scss" scoped>
 .layout-wrapper {
