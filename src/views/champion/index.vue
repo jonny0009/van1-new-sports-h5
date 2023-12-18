a18c80390aab37753bbcfad241c53c4ae649feff
ricky <XY@gmail.com>
2023-12-18 15:25:30 +0800

    添加冠军页面

---
+++ src/views/champion/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="champion-page">
+    <div class="title">选择联赛</div>
+    <ul>
+      <li>足球</li>
+      <li>足球</li>
+      <li>足球</li>
+      <li>足球</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+console.log(1234)
+</script>
+
+<style lang="scss">
+
+</style>
