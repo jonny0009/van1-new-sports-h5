0d51004e3ae42a79a9c127e21dca6eaa32952c4b
van <XY@gmail.com>
2023-12-21 23:33:57 +0800

    完善注单

--- src/components/BettingSlip/components/ActionBar/index.vue
+++ src/components/BettingSlip/components/ActionBar/index.vue
@@ -1,19 +1,33 @@
 <template>
+  <div v-if="mode === 2" ref="inputBtn" class="combo-input betting-slip-input" @click="inputTouch">
+    <span class="currency"><van-icon name="balance-o" /></span>
+    <div style="flex: 1 1 0%;"></div>
+    <span class="amount" :class="{ selected:boardShow }">{{ comboAmount }}</span>
+    <span v-show="boardShow" class="cursor">|</span>
+  </div>
   <div class="action-bar">
     <div class="bet-golds">
       <span class="title">{{ $t('betting.betGolds') }} ：</span>
-      <span class="value">{{ betsGolds }}</span>
+      <span v-points="golds" class="value"></span>
     </div>
     <div class="bet-profit">
       <span class="title">{{ $t('betting.betProfit') }} ：</span>
-      <span class="value">{{ betsProfit }}</span>
+      <span v-points="profit" class="value"></span>
     </div>
     <div v-if="errorTips" class="error-tips">
       {{ errorTips }}
     </div>
     <div class="confirm-button-wrap">
       <SvgIcon v-if="hitState === 1" name="betting-trash" class="delete-btn" @click="clear"></SvgIcon>
-      <div v-if="hitState === 1" v-debounce="buy" class="confirm-button">{{ $t('betting.buy') }}</div>
+      <button
+        v-if="hitState === 1"
+        v-debounce="buy"
+        type="button"
+        class="confirm-button"
+        :disabled="effectiveMarkets.length === 0"
+      >
+        {{ $t('betting.buy') }}
+      </button>
       <div v-else class="confirm-button">
         <span class="loading-icon"></span>
       </div>
@@ -27,17 +41,31 @@ const { t } = useI18n()
 import store from '@/store'
 const errorTips = ref()
 const errorTimer = ref()
+const boardShow = computed(() => store.state.betting.boardShow)
 const hitState = computed(() => store.state.betting.hitState)
+const mode = computed(() => store.state.betting.mode)
+const comboAmount = computed(() => store.state.betting.comboAmount)
 const betsGolds = computed(() => store.getters['betting/betsGolds'])
 const betsProfit = computed(() => store.getters['betting/betsProfit'])
+const combosBetGolds = computed(() => store.getters['betting/combosBetGolds'])
+const combosProfit = computed(() => store.getters['betting/combosProfit'])
+const effectiveMarkets = computed(() => store.getters['betting/effectiveMarkets'])
+const golds = computed(() => mode.value === 1 ? betsGolds.value : combosBetGolds.value)
+const profit = computed(() => mode.value === 1 ? betsProfit.value : combosProfit.value)
 
 const clear = () => {
   store.dispatch('betting/clearMarkets')
 }
 const buy = () => {
-  store.dispatch('betting/marketBetting').catch((err: any) => {
-    error(err || t('betting.errorTips'))
-  })
+  if (mode.value === 1) {
+    store.dispatch('betting/marketBetting').catch((err: any) => {
+      error(err || t('betting.errorTips'))
+    })
+  } else {
+    store.dispatch('betting/comboMarketBetting').catch((err: any) => {
+      error(err || t('betting.errorTips'))
+    })
+  }
 }
 const error = (err: string) => {
   errorTips.value = err
@@ -46,13 +74,74 @@ const error = (err: string) => {
     errorTips.value = ''
   }, 3000)
 }
+const inputBtn = ref()
+const inputTouch = () => {
+  store.dispatch('betting/setBoardShow', { status: true })
+  setTimeout(() => {
+    inputBtn.value.scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+}
+
 </script>
 <style lang="scss" scoped>
+@keyframes auto-opacity {
+  0% {
+    opacity: 1
+  }
+
+  to {
+    opacity: 0
+  }
+}
+
+.combo-input {
+  display: flex;
+  align-items: center;
+  width: 676px;
+  height: 50px;
+  margin: 20px auto 0;
+  border: 2px solid rgba(118, 66, 253, 1);
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 5px;
+  padding-right: 11px;
+  background-color: #fff;
+  font-family: PingFangSC-Semibold;
+  font-size: 24px;
+  color: #000000;
+  letter-spacing: 0.8px;
+  text-align: justify;
+  font-weight: 600 ;
+  .amount {
+    font-family: PingFangSC-Semibold;
+    font-size: 24px;
+    color: #000000;
+    letter-spacing: 0.8px;
+    text-align: justify;
+    font-weight: 600;
+
+    &.selected {
+      color: #dfe4e5;
+    }
+  }
+
+  .cursor {
+    position: relative;
+    right: -4px;
+    top: -3px;
+    animation: auto-opacity .5s linear infinite alternate;
+  }
+}
+
 .action-bar {
   position: relative;
   display: flex;
   flex-direction: column;
-  padding: 50px 38px 20px;
+  padding: 22px 38px 20px;
 
   .error-tips {
     position: absolute;
@@ -140,6 +229,11 @@ const error = (err: string) => {
       background-color: #7440fe;
       color: #fff;
       border-radius: 40px;
+      border: none;
+
+      &:disabled {
+        opacity: 0.7;
+      }
 
       .loading-icon {
         display: inline-block;
