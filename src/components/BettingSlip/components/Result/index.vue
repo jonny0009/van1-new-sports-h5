a86f83874cd093d1f23e9dc4e972c184ed80241d
van <XY@gmail.com>
2023-12-21 16:19:10 +0800

    完善注单

--- src/components/BettingSlip/components/Result/index.vue
+++ src/components/BettingSlip/components/Result/index.vue
@@ -1,28 +1,53 @@
 <template>
-  <div class="betting-slip-empty">
-    <EmptyIcon />
-    <div class="empty-title">1111111111111111111</div>
+  <div class="result-page">
+    <div class="result-item">
+      <div class="title">
+        <span class="state-icon" :class="`${'check'}`"></span>
+        {{ }}
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 
 </script>
 <style scoped lang="scss">
-.betting-slip-empty {
+.result-page {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 50px 0;
 
-  .empty-title {
-    margin-top: 10px;
-    font-family: PingFangSC-Medium;
-    font-size: 24px;
-    color: #96A5AA;
-    letter-spacing: 0;
-    text-align: center;
-    font-weight: 500;
+  .result-item {
+
+    .title {
+      display: flex;
+      align-items: center;
+      font-family: PingFangSC-Medium;
+      font-size: 24px;
+      color: #000000;
+      letter-spacing: 0;
+      font-weight: 500;
+
+      .state-icon {
+        display: inline-block;
+        width: 23px;
+        height: 23px;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+
+        &.check {
+          background-image: url('@/assets/images/betting/check-state.png');
+        }
+
+        &.error {
+          background-image: url('@/assets/images/betting/close-state.png');
+
+        }
+      }
+    }
   }
 }
 </style>
