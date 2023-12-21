05c1fbca3eb4bc71b9a09829d6e061471d5d0d70
ricky <XY@gmail.com>
2023-12-21 16:06:51 +0800

    足球页

--- src/views/sport/index.vue
+++ src/views/sport/index.vue
@@ -2,11 +2,18 @@
   <div class="sport-page">
     <div class="my-scroll__content">
       <div class="betting-sport-nav">
-        <TextButton text="推荐" :active="true" />
-        <ImageButton v-for="item in [1,2,3,4,5]" :key="item" text="和恢复顶顶顶帆帆" src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" :active="true" />
+        <TextButton text="推荐" :active="!leagueId" @click="clickLeague({})" />
+        <ImageButton
+          v-for="(item,idx) in firstLeaguesList"
+          :key="idx"
+          :text="item.leagueNameAbbr"
+          :src="item.icon"
+          :active="leagueId===item.leagueId"
+          @click="clickLeague(item)"
+        />
       </div>
     </div>
-    <MatchLive />
+    <HomeMatchHandicap />
     <ChampionList v-if="championList.length" :champion-list="championList" />
 
     <div class="Button-MatchMore mt20">
@@ -19,24 +26,51 @@
   </div></template>
 
 <script lang="ts" setup>
-import MatchLive from '@/components/HomeMatch/MatchLive/index.vue'
+import HomeMatchHandicap from '@/components/HomeMatch/MatchHandicap/index.vue'
 import ChampionList from './champion/index.vue'
 import TextButton from '@/components/Button/TextButton/index.vue'
 import ImageButton from '@/components/Button/ImageButton/index.vue'
 import { useRoute } from 'vue-router'
+// import router from '@/router'
 import { ref, onBeforeMount } from 'vue'
 import { apiChampionpPlayTypes } from '@/api/champion'
-import { MarketInfo } from '@/entitys/MarketInfo'
+import { firstLeagues, recommendEvents } from '@/api/home'
 
+import { MarketInfo } from '@/entitys/MarketInfo'
+const route = useRoute()
+const chooseLeagueId: any = ref()
+const leagueId: any = ref(route.query.leagueId)
+const gameType: any = ref(route.query.type)
 onBeforeMount(async () => {
-  console.log(leagueId.value)
+  getFirstLeagues()
+  getRecommendEvents()
   getChampionpPlayTypes()
 })
 
-const route = useRoute()
-const leagueId: any = ref(route.query.leagueId)
-const championList: any = ref([])
+const firstLeaguesList: any = ref([])
+// 获取一级联赛
+const getFirstLeagues = async () => {
+  if (gameType.value) {
+    const res:any = await firstLeagues({ gameType: gameType.value }) || {}
+    if (res.code === 200 && res.data) {
+      firstLeaguesList.value = res.data
+    }
+  }
+}
 
+const recommendList: any = ref([])
+const earlyList: any = ref([])
+// 获取推荐，早盘赛事
+const getRecommendEvents = async () => {
+  if (gameType.value) {
+    const res:any = await recommendEvents({ gameType: gameType.value }) || {}
+    if (res.code === 200 && res.data) {
+      firstLeaguesList.value = res.data
+    }
+  }
+}
+
+const championList: any = ref([])
 // 获取冠军
 const getChampionpPlayTypes = async () => {
   if (leagueId.value) {
@@ -62,6 +96,15 @@ const getChampionpPlayTypes = async () => {
   }
 }
 
+const clickLeague = (item: any) => {
+  // chooseLeagueId.value = item.leagueId
+  if (item.leagueId) {
+    window.location.href = '/sport?leagueId=' + item.leagueId + '&type=' + item.gameType
+  } else {
+    window.location.href = '/sport?type=' + gameType.value
+  }
+}
+
 </script>
 
 <style lang="scss">
@@ -78,9 +121,9 @@ const getChampionpPlayTypes = async () => {
     }
     .betting-sport-nav{
       margin-top: 26px;
-      margin-left: 21px;
+      // margin-left: 21px;
       margin-bottom: 5px;
-      width: 1000px;
+      width: 1600px;
       white-space: normal;
       position: relative;
       .textButton,
