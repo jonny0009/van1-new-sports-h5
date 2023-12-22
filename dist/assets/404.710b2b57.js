984fd64238c5aaed2ffcd208c262e8b4364d5a1f
XY-Chaos <XY-Chaos@gmail.com>
2023-12-22 19:32:42 +0800

    打包

--- dist/assets/404.710b2b57.js
+++
@@ -1,1 +0,0 @@
-import{o as r,u as e}from"./vue.3b4f9705.js";const n={};n.render=function(n,o){return r(),e("div",null,"404")};export{n as default};
--- dist/assets/index.7fe86dc5.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index.7c624dac.js";export{_ as default}from"./index.7c624dac.js";import"./vue.3b4f9705.js";
