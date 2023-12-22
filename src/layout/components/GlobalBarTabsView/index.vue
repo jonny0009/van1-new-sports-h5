d30833a53736d355209ede6c58ba3ae9a9fb4752
XY-Chaos <XY-Chaos@gmail.com>
2023-12-22 13:42:46 +0800

    首页 接入

--- src/layout/components/GlobalBarTabsView/index.vue
+++ src/layout/components/GlobalBarTabsView/index.vue
@@ -24,31 +24,28 @@ import defaultTime from '@/assets/images/home/homeTabs/homeTabs-default-time.png
 import time from '@/assets/images/home/homeTabs/homeTabs-time.png'
 import important from '@/assets/images/home/homeTabs/homeTabs-important.png'
 import defaultImportant from '@/assets/images/home/homeTabs/homeTabs-default-important.png'
-import { ref, watch, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import router from '@/router'
-const getRouteName = () => {
+const active = computed(() => {
+  const getRouteNameObj:any = {
+    'home': 'ImportantRecommend',
+    'hometime': 'TimeSort',
+    'champion': 'ChampionBet'
+  }
   const routerName: any = router?.currentRoute?.value?.name || ''
-  return routerName.toLowerCase()
-}
-const getRouteNameObj:any = {
-  'home': 'ImportantRecommend',
-  'hometime': 'TimeSort',
-  'champion': 'ChampionBet'
-}
-const active = ref(getRouteNameObj[getRouteName()])
-const currentRoute = computed(() => router?.currentRoute?.value?.name)
-watch(currentRoute, (val) => {
+  const newRouterName = routerName.toLowerCase()
+  const val = getRouteNameObj[newRouterName]
+  return val
+})
+
+watch(active, (val) => {
   if (val) {
-    active.value = getRouteNameObj[getRouteName()]
-    console.log(active.value, 'active.value active.value')
-    console.log(homeBarArrayTable(active.value), 'assctive.value active.value')
+    homeBarArray.value.length = 0
+    homeBarArray.value.push(...homeBarArrayTable(active.value))
   }
 })
 const clickChangeActive = (item:any) => {
-  const { value, name } = item
-  homeBarArray.value.length = 0
-  homeBarArray.value.push(...homeBarArrayTable(value))
-  active.value = value
+  const { name } = item
   const params:any = { name }
   router.push(params)
 }
