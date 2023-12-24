f36697743ec315e6261c6fa3555bb8da6683be29
ricky <XY@gmail.com>
2023-12-24 15:17:08 +0800

    build

--- dist/assets/index.6b199686.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.e96292d5.js";export{_ as default}from"./index.e96292d5.js";import"./vue.3b4f9705.js";
---
+++ dist/assets/index.877c609e.js
@@ -0,0 +1,1 @@
+import{r,o as e,c as o}from"./vue.85a22cc6.js";const t={};t.render=function(t,s){const a=r("router-view");return e(),o(a)};export{t as default};
