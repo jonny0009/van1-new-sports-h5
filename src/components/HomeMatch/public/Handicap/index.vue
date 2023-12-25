8e03782a595ae084343530a7604527ab47bfd549
XY-Chaos <XY-Chaos@gmail.com>
2023-12-25 15:54:31 +0800

    首页 交互

--- src/components/HomeMatch/public/Handicap/index.vue
+++ src/components/HomeMatch/public/Handicap/index.vue
@@ -6,19 +6,28 @@
     :market-info="item.marketInfo"
     class="betting-option"
   >
-    <span class="up-betting-name">
-      <span class="text">
-        <span class="team">
-          {{ item.marketInfo.ratioMatch || item.marketInfo.ratioName }}
-        </span>
-        <span v-if="item.marketInfo.ratioTag" class="ratio">
-          {{ item.marketInfo.ratioTag }}
+    <template #default="scope">
+      <!-- <i v-if="scope.lock" class="lock"></i> -->
+      <!--
+        1. up-ior
+        2. down-ior
+       -->
+      <!-- <i v-if="scope.iorChange" :class="scope.iorChange"></i> -->
+      <i class="lock"></i>
+      <span class="up-betting-name">
+        <span class="text">
+          <span class="team">
+            {{ item.marketInfo.ratioMatch || item.marketInfo.ratioName }}
+          </span>
+          <span v-if="item.marketInfo.ratioTag" class="ratio">
+            {{ item.marketInfo.ratioTag }}
+          </span>
         </span>
       </span>
-    </span>
-    <div class="details">
-      <div class="item">{{ item.marketInfo.vior }}</div>
-    </div>
+      <div class="details">
+        <div class="item">{{ item.marketInfo.vior }}</div>
+      </div>
+    </template>
   </BettingOption>
 </template>
 <script lang="ts" setup>
--- src/utils/home/getHandicap.ts
+++ src/utils/home/getHandicap.ts
@@ -5,7 +5,6 @@ export function getHandicap(playType: string, sendParams:any) {
   let newPlayTypeItem:any = {}
   const newRatioDataVal:any = playTypeItem?.ratioData || []
   if (['R', 'RE'].includes(playType)) {
-    // REC //REH
     const RC = newRatioDataVal.find((e:any) => {
       return ['REC', 'RC'].includes(e.ratioType)
     })
@@ -17,19 +16,22 @@ export function getHandicap(playType: string, sendParams:any) {
       ratioData: [...[RH, RC]]
     }
   } else if (['M', 'RM'].includes(playType)) {
-    // RMC //RMH
-    const MN = newRatioDataVal.find((e:any) => {
-      return ['RMN', 'MN'].includes(e.ratioType)
-    })
     const MC = newRatioDataVal.find((e:any) => {
       return ['RMC', 'MC'].includes(e.ratioType)
     })
     const MH = newRatioDataVal.find((e:any) => {
       return ['RMH', 'MH'].includes(e.ratioType)
     })
+    const MN = newRatioDataVal.find((e:any) => {
+      return ['RMN', 'MN'].includes(e.ratioType)
+    })
+    let newRatioData = [MH, MN, MC]
+    if (!MN) {
+      newRatioData = [MH, MC]
+    }
     newPlayTypeItem = {
       ...playTypeItem,
-      ratioData: [...[MH, MN, MC]]
+      ratioData: [...newRatioData]
     }
   } else if (['OU', 'ROU'].includes(playType)) {
     const OUH = newRatioDataVal.find((e:any) => {
