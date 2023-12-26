b63479a93790e6d68d922787df3a30b55ae727b4
van <XY@gmail.com>
2023-12-26 20:32:14 +0800

    build

--- dist/assets/index.57ba492b.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index.f7b9dba8.js";export{_ as default}from"./index.f7b9dba8.js";import"./vue.85a22cc6.js";
---
+++ dist/assets/index.679204e1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.3fdc477b.js";export{_ as default}from"./index.3fdc477b.js";import"./vue.e791136f.js";
