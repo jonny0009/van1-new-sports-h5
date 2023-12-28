9891946d1df507264637d924690ad8064b96d13e
Ron660 <cgh88Qpl@gmail.com>
2023-12-28 10:39:16 +0800

    搜索

--- src/views/search/components/match.vue
+++ src/views/search/components/match.vue
@@ -99,6 +99,7 @@ const highlightText = (field: any, text: any) => {
 
 <style lang="scss" scoped>
 .noData {
+  width: 100%;
   text-align: center;
   font-family: PingFangSC-Medium;
   font-size: 24px;
