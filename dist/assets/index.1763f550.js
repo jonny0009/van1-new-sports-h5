3a6457135d6f276a94b7fac64cb8d2aff36263ce
Ron660 <cgh88Qpl@gmail.com>
2023-12-29 16:28:16 +0800

    修改多语言

--- dist/assets/index.1763f550.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.adcabc5b.js";const e=e=>a({url:"/ai/mobile/matches/firstLeagues",method:"get",params:e}),m=e=>a({url:"/ai/mobile/recommend/events",method:"get",params:e}),t=e=>a({url:"/ai/mobile/matches/getRBCondition",method:"get",params:e}),s=e=>a({url:"/ai/mobile/matches/commonMatches",method:"get",params:e});export{t as a,s as b,e as f,m as r};
--- dist/assets/index.adc71086.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./index.adcabc5b.js";export{f as default}from"./index.adcabc5b.js";import"./vue.979f7bee.js";
