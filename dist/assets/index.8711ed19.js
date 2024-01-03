66db6c114768a998bc1b62ea1d9b098a5acf1e3b
van <XY@gmail.com>
2024-01-03 15:26:11 +0800

    修改bug

--- dist/assets/index.8711ed19.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./index.aa5abac0.js";export{f as default}from"./index.aa5abac0.js";import"./vue.e791136f.js";
--- dist/assets/index.c9fa167f.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.aa5abac0.js";const e=()=>a({url:"/ai/mobile/game/champion/gameTypes",method:"get"}),m=e=>a({url:"/ai/mobile/game/champion/championLeagueInfo",method:"get",params:e}),o=e=>a({url:"/ai/mobile/game/champion/playTypes/v2",method:"get",params:e});export{e as a,m as b,o as c};
