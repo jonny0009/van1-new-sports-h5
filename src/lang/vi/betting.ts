abe0ffb52183c1ba8bdf2783df36e788cbfd42c8
van <XY@gmail.com>
2024-01-03 11:00:52 +0800

    翻译

--- src/lang/vi/betting.ts
+++ src/lang/vi/betting.ts
@@ -11,5 +11,12 @@ export default {
   betting: 'Đang đặt cược...',
   errorTips: 'Mạng bất thường！',
   orderCheck: 'Cược được chấp nhận',
-  orderError: 'Cược không được chấp nhận'
+  orderError: 'Cược không được chấp nhận',
+  finish: 'Hoàn thành',
+  oddChangTips: 'Chấp nhận tất cả các thay đổi cược',
+  notPendingOrders: 'Không có đơn cược đang chờ',
+  notBets: 'Lựa chọn của bạn sẽ xuất hiện ở đây',
+  combos: '{num} cược kết hợp',
+  acceptOdds: 'Chấp nhận tỷ lệ cược',
+  eventClosure: 'Đóng cửa sự kiện'
 }
