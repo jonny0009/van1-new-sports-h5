4ced00b5552de5e2df8bbaacec852c5e7428f257
van <XY@gmail.com>
2024-01-04 10:56:48 +0800

    修改bug

--- dist/assets/index.8cd9efda.js
+++
@@ -1,1 +0,0 @@
-import"./index.cd681f2f.js";import{s,d as a,M as e,o as d,R as o}from"./vue.e791136f.js";const t={class:"game-page"};var c=s({setup:s=>(a(""),e((async()=>{})),(s,a)=>(d(),o("div",t)))});c.__scopeId="data-v-c97b24e4";export{c as default};
--- dist/assets/index.95537819.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index.cd681f2f.js";export{f as default}from"./index.cd681f2f.js";import"./vue.e791136f.js";
