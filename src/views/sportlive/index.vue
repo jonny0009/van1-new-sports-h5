91581c876cb9f2f1c9a75a54cbece7cd5e4786d1
XY-Chaos <XY-Chaos@gmail.com>
2024-01-04 17:23:29 +0800

    首页

--- src/views/sportlive/index.vue
+++ src/views/sportlive/index.vue
@@ -4,6 +4,9 @@
       <TextButton :text="$t('sport.all')" :active="!gameType" @click="clickGameType({})" />
       <SportsButton v-for="(item,idx) in gameTypeList" :key="idx" :text="item.gameType" :active="gameType===item.gameType" @click="clickGameType(item)" />
     </div>
+
+    <swipeLive />
+
     <Loading v-if="!isLoading" />
     <template v-else>
       <MatchLive v-for="(item,idx) in commonMatchesList" :key="idx" :send-params="item" />
@@ -20,6 +23,7 @@
 <script lang="ts" setup>
 import TextButton from '@/components/Button/TextButton/index.vue'
 import MatchLive from '@/components/HomeMatch/MatchLive/index.vue'
+import swipeLive from './swipeLive/index.vue'
 import store from '@/store'
 import { ref, onBeforeMount, onActivated, onDeactivated, onBeforeUnmount, computed, watch } from 'vue'
 import { apiRBCondition, apiCommonMatches } from '@/api/home'
---
+++ src/views/sportlive/swipeLive/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="swipeLive">
+    <!--  -->
+    swipeLive swipeLive swipeLive swipeLive swipeLive
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
