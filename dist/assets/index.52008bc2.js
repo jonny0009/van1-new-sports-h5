b029a565a6b1de492deef83cb48866865b8812ab
Bevis <be69385@gmail.com>
2024-01-04 11:51:05 +0800

    打包

---
+++ dist/assets/index.52008bc2.js
@@ -0,0 +1,1 @@
+import"./index.3b6c2c2d.js";import{s as a,d as s,M as e,o as d,R as o}from"./vue.93da156d.js";const t={class:"game-page"};var c=a({setup:a=>(s(""),e((async()=>{})),(a,s)=>(d(),o("div",t)))});c.__scopeId="data-v-8e32e3ee";export{c as default};
