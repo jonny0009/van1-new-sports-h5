4b53c796eedc60a1201faf4dd50a12178e639945
van <XY@gmail.com>
2024-01-04 12:11:13 +0800

    修改bug

--- dist/assets/index.33798319.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index.92df0468.js";export{f as default}from"./index.92df0468.js";import"./vue.e791136f.js";
---
+++ dist/assets/index.88d35347.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.ea0f2fab.js";export{f as default}from"./index.ea0f2fab.js";import"./vue.e791136f.js";
