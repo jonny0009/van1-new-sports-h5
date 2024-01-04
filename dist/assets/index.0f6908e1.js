2f6bdda7092c3c12222b15215f9757027b822f94
van <XY@gmail.com>
2024-01-04 11:10:44 +0800

    修改bug

---
+++ dist/assets/index.0f6908e1.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.e8b4260a.js";export{f as default}from"./index.e8b4260a.js";import"./vue.e791136f.js";
--- dist/assets/index.80ea560c.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index.186de241.js";export{f as default}from"./index.186de241.js";import"./vue.e791136f.js";
