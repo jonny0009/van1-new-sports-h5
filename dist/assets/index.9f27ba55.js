7bdbbde4f27d68dcdf1b38ca7131136aaed04a9b
van <XY@gmail.com>
2024-01-08 20:24:00 +0800

    修改bug

---
+++ dist/assets/index.9f27ba55.js
@@ -0,0 +1,1 @@
+import{g as a}from"./index.fa69a8fe.js";export{g as default}from"./index.fa69a8fe.js";import"./vue.b83303fa.js";
--- dist/assets/index.bec24ff7.js
+++
@@ -1,1 +0,0 @@
-import{g as d}from"./index.072c81cd.js";export{g as default}from"./index.072c81cd.js";import"./vue.45f01342.js";
