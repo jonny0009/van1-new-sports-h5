6075c15bff512b6cfa2e7c5ce041e2622f5cb69d
Ron660 <cgh88Qpl@gmail.com>
2024-01-08 14:04:09 +0800

    韩语翻译

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -2,15 +2,20 @@ import { createI18n } from 'vue-i18n'
 import en from './en/index'
 import cn from './cn/index'
 import vi from './vi/index'
+import ko from './ko/index'
 const lang = localStorage.getItem('locale') || 'zh-cn'
 import { Locale } from 'vant'
 import enUS from 'vant/es/locale/lang/en-US'
 import viVN from 'vant/es/locale/lang/vi-VN'
+import koKR from 'vant/es/locale/lang/ko-KR'
 if (lang === 'en-us') {
   Locale.use('en-US', enUS)
 }
 if (lang === 'vi-vn') {
   Locale.use('vi-VN', viVN)
+}
+if (lang === 'ko-kr') {
+  Locale.use('ko-KR', koKR)
 }
 const i18n: any = createI18n({
   legacy: false,
@@ -19,7 +24,8 @@ const i18n: any = createI18n({
   messages: {
     'zh-cn': cn,
     'en-us': en,
-    'vi-vn': vi
+    'vi-vn': vi,
+    'ko-kr': ko
   }
 })
 
