0cdb69e65ab5f96da36b54649dcf58b1ad88e907
Ron660 <cgh88Qpl@gmail.com>
2024-01-09 11:27:17 +0800

    登录优化

--- dist/assets/index.c4f920b5.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.fa69a8fe.js";const a=a=>e({url:"/ai/mobile/matches/firstLeagues",method:"get",params:a}),m=a=>e({url:"/ai/mobile/recommend/events",method:"get",params:a}),t=a=>e({url:"/ai/mobile/matches/getRBCondition",method:"get",params:a}),s=a=>e({url:"/ai/mobile/matches/commonMatches",method:"get",params:a});export{t as a,s as b,a as f,m as r};
