e145ecf5a3a22795bf14fe2ff8ce5368957c4dc6
van <XY@gmail.com>
2024-01-10 20:19:41 +0800

    修改bug

---
+++ dist/assets/index.09c30ca8.js
@@ -0,0 +1,1 @@
+import{g as a}from"./index.fcaa5b99.js";export{g as default}from"./index.fcaa5b99.js";import"./vue.469c5994.js";
--- dist/assets/index.c95f872b.js
+++
@@ -1,1 +0,0 @@
-import{g as a}from"./index.d01fa8c7.js";export{g as default}from"./index.d01fa8c7.js";import"./vue.469c5994.js";
