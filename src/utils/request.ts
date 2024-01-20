dc4bd28c8a7e99d4440f006b237797a170227e80
Bevis <be69385@gmail.com>
2024-01-20 18:18:17 +0800

    url token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -11,7 +11,7 @@ const service = axios.create({
   timeout: 30000 // request timeout
 })
 // 不需要传groupId参数的数组
-const noGroupId:Array<string> = [
+const noGroupId: Array<string> = [
   '/order/all/betRecordTab',
   '/common/businessConfig',
   '/merchantAccountServer/api/c/getCMerAccessType'
@@ -55,8 +55,8 @@ const authCode: any = [401, 403, 1010]
 service.interceptors.response.use(
   (response: any) => {
     if (authCode.includes(response.data.code)) {
-      removeToken()
-      const locale:any = localStorage.getItem('locale') || 'zh-cn'
+      // removeToken()
+      const locale: any = localStorage.getItem('locale') || 'zh-cn'
       const inform: any = {
         'zh-cn': '登录信息已失效,请重新登录',
         'vi-vn': 'Thông tin đăng nhập đã hết hạn, vui lòng đăng nhập lại',
@@ -68,6 +68,7 @@ service.interceptors.response.use(
         message: inform[locale],
         theme: 'round-button'
       }).then(() => {
+        removeToken()
         router.push('/login')
       })
       // router.push('/login')
