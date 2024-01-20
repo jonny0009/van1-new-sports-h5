aa12c9443b5cfbd127e08a7b7aee95ed259c21fc
Bevis <be69385@gmail.com>
2024-01-20 22:15:05 +0800

    默认语言

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,15 +1,49 @@
 import { createI18n } from 'vue-i18n'
+import { getURLSearchParams } from '@/utils'
 import en from './en/index'
 import cn from './cn/index'
 import vi from './vi/index'
 import kr from './kr/index'
 import pt from './pt/index'
-const lang = localStorage.getItem('locale') || 'zh-cn'
+
 import { Locale } from 'vant'
 import enUS from 'vant/es/locale/lang/en-US'
 import viVN from 'vant/es/locale/lang/vi-VN'
 import koKR from 'vant/es/locale/lang/ko-KR'
 import ptBR from 'vant/es/locale/lang/pt-BR'
+
+const searchParams = getURLSearchParams()
+const getBrowserLanguage = () => {
+  if (window.navigator && window.navigator.language) {
+    const obj: any = {
+      'zh-cn': 'zh-cn',
+      'zh-tw': 'zh-tw',
+      'en-us': 'en-us',
+      'vi-vn': 'vi-vn',
+      'th-th': 'th-th',
+      'ko-kr': 'ko-kr',
+      'pt-pt': 'pt-pt',
+      en: 'en-us',
+      vi: 'vi-vn',
+      th: 'th-th',
+      ko: 'ko-kr',
+      pt: 'pt-pt'
+    }
+
+    let sysLanguage = ''
+    if (searchParams && searchParams.lang) {
+      sysLanguage = searchParams.lang
+    } else {
+      sysLanguage = window.navigator.language.toLowerCase()
+    }
+
+    return obj[sysLanguage]
+  }
+  return 'en-us'
+}
+
+const lang = localStorage.getItem('locale') || getBrowserLanguage() // 'zh-cn'
+
 if (lang === 'en-us') {
   Locale.use('en-US', enUS)
 }
