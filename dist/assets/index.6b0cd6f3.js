cc1ef5d15cf2619e498cf7bd1a2508a9f319c2c5
huihui <huihui>
2024-01-26 20:56:48 +0800

    build

--- dist/assets/index.6b0cd6f3.js
+++
@@ -1,1 +0,0 @@
-import{r as e,o as r,c as a}from"./vue.7b2d67a6.js";const o={};o.render=function(o,t){const s=e("router-view");return r(),a(s)},o.__scopeId="data-v-697caec7";export{o as default};
---
+++ dist/assets/index.ad5040e1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.5ce50534.js";export{_ as default}from"./index.5ce50534.js";import"./vue.66060cab.js";
