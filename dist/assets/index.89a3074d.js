20fa125c60d1bd3d3e490317212afc54e57d9e27
Ron660 <cgh88Qpl@gmail.com>
2024-02-02 17:48:42 +0800

    翻译

--- dist/assets/index.89a3074d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.vue_vue_type_script_setup_true_lang.ecf87aec.js";import{s,o as a,S as t,q as p}from"./vue.7b2d67a6.js";import"./index.4ce0608b.js";import"./index.eb36fce9.js";const o={class:"panel-data"};var r=s({setup:s=>(s,r)=>(a(),t("div",o,[p(e)]))});r.__scopeId="data-v-2e354294";export{r as default};
