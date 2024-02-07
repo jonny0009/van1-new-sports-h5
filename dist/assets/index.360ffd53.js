40d699563ff030080384a8e6efdd1c138a80f07e
van <huihui>
2024-02-07 15:10:53 +0800

    修改bug

--- dist/assets/index.360ffd53.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./index.30c489e6.js";export{e as default}from"./index.30c489e6.js";import"./vue.9399d1f0.js";
---
+++ dist/assets/index.b2155aad.js
@@ -0,0 +1,1 @@
+import{e as d}from"./index.34db2935.js";export{e as default}from"./index.34db2935.js";import"./vue.9399d1f0.js";
