5e7c8bfead3100ca5d4480b49f942ea010dfef25
coke <coke@ecoph.tk>
2024-03-04 12:21:36 +0800

    比分

--- src/lang/en/live.ts
+++ src/lang/en/live.ts
@@ -66,5 +66,8 @@ export default {
   win: 'win',
   tie: 'flat',
   lose: 'negative',
-  point: 'point'
+  point: 'point',
+  nextAM: 'The next live broadcast will start on <span class="time-num">{num}</span>',
+  halfPlay: 'halftime',
+  section: 'section'
 }
