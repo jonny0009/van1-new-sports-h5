f546389c65526266f8aedbdfa503f8c8ebecb686
Ron660 <cgh88Qpl@gmail.com>
2024-04-05 18:40:51 +0800

    首页早盘优化

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -62,8 +62,7 @@ const appModule: Module<App, any> = {
       const res: any = (await merchantConfig()) || {}
       if (res.code === 200) {
         state.merchantConfig = res.data || {}
-        // state.homeStyle = res.data?.homeStyle || 2
-        state.homeStyle = 2
+        state.homeStyle = res.data?.homeStyle || 2
       }
     },
     async moduleConfig({ state }) {
