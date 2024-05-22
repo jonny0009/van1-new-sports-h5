d6c88d7b1c635c9a02d4a398d4555ae52be231bd
Ron660 <cgh88Qpl@gmail.com>
2024-05-22 13:08:46 +0800

    球类图片

--- src/views/match/ListItem.vue
+++ src/views/match/ListItem.vue
@@ -83,14 +83,12 @@ const setMatch: any = useMatch()
 const gameInfo = computed(() => props.item?.gameInfo)
 const imgCover = computed(() => {
   const item = props.item
-  if (!item.anchorId) {
-    if (item.gameType == 'BK') {
-      return ImageSource + 'FE/common/live/img_video_bg_BK.jpg'
-    } else {
-      return ImageSource + 'FE/common/live/img_video_bg_FT.jpg'
-    }
+  if (item.cover) {
+    return ImageSource + item.cover
   }
-  return ImageSource + item.cover
+  const gameType = item.gameType || 'FT'
+  const mask = item.showType !== 'RB' ? '_mask' : ''
+  return ImageSource + `FE/common/live/VIDEO_BG_${gameType}${mask}.jpg`
 })
 
 const watchNumText = computed(() => {
