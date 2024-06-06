2efc5fc2b0eb05227c4e9f847060417935dbe4d2
Beer <ema320916@gmail.com>
2024-06-06 19:35:41 +0800

    feat: build 1.4

--- dist/assets/index.2156183a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.vue_vue_type_script_setup_true_lang.b4feeac0.js";export{_ as default}from"./index.vue_vue_type_script_setup_true_lang.b4feeac0.js";import"./index.cb503d4e.js";import"./vue.7833d4f2.js";
---
+++ dist/assets/index.7c3be147.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.f78e2000.js";export{f as default}from"./index.f78e2000.js";import"./vue.2cb7924b.js";
